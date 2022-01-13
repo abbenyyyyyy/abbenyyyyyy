4956a2ca26f7ef6da2aa1156e7850e926e68dd1d
Benzan.Chen <abbenyyyyyy@qq.com>
2022-01-13 21:12:24 +0800

    update action

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const buildNewReadme = (previousContent, newContent) => {
       startOfOpening + startTag.length,
       endOfOpening
     );
+    core.notice(`原文本:${willReplaceContent}`);
     // 比对新的 readme 文本 与 旧的是否一致
     if (willReplaceContent === newContent) {
       return null;
@@ -102,6 +103,7 @@ const main = async () => {
   const readmeData = fs.readFileSync(README_FILE_PATH, "utf-8");
   const newReadme = buildNewReadme(readmeData, addReadme);
   if (newReadme) {
+    core.notice(`写入新文本:${newReadme}`);
     fs.writeFileSync(README_FILE_PATH, newReadme);
     // 将变化提交到github
     const GITHUB_TOKEN = core.getInput("gh_token");
@@ -109,6 +111,7 @@ const main = async () => {
     const COMMITTER_EMAIL = core.getInput("committer_email");
     // 将 GITHUB_TOKEN 设为秘密，不在日志中显示
     core.setSecret(GITHUB_TOKEN);
+    core.notice('执行 git 设置命令');
     await exec("git", ["config", "--global", "user.email", COMMITTER_EMAIL]);
     if (GITHUB_TOKEN) {
       await exec("git", [
